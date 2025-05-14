3ce7843b7d2dcbc7189495ab74220d1f4a06bd37
seetaramoruganti <orugantiseetaram@gmail.com>
2025-05-14 07:56:34 -0400

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="mt-3">Go Watch a Movie!</h1>
+        </div>
+        <div className="col text-end">
+          <a href="#!">
+            <span className="badge bg-success">Login</span>
+          </a>
+        </div>
+        <hr className="mb-3"></hr>
+      </div>
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <div className="list-group">
+              <a href="#!" className="list-group-item list-group-item-action">
+                Home
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                Movies
+              </a>
+            </div>
+          </nav>
+        </div>
+        <div className="col-md-10"></div>
+      </div>
     </div>
   );
 }
-
 export default App;
